1cf87ca57d568409a48d936778e2f0fdef45babb
Lizu-4 <liz.ugas@correo.unimet.edu.ve>
2023-01-24 16:49:46 -0400

    Listo:sparkles:

--- src/models.py
+++ src/models.py
@@ -8,23 +8,45 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(40), nullable=False)
+    password = Column(String(10), nullable=False)
+    mail = Column(String(50), nullable=False)
+    favorites = relationship('favorite')
+
+
+class Favorites(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    planets_id = Column(Integer, ForeignKey("planets.id"), nullable=True)
+    person_id = Column(Integer, ForeignKey("person.id"), nullable=True)
+    
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    gender = Column(String(6))
+    birth_year = Column(Integer)
+    children = relationship("Favorites")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer)
+
 
     def to_dict(self):
         return {}
